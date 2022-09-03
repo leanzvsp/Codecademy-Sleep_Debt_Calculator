ee30f1e09493fe7aab40bcc2330cddfa1eb9909b
Leandro Marin <marinlopezleandro@gmail.com>
2022-09-02 23:59:33 -0300

    Adding hours in every day of the week

---
+++ README.md
@@ -0,0 +1,3 @@
+**Sleep Debt Calculator**
+
+The program will determine the actual and ideal hours of sleep for each night of the last week.
---
+++ sleepDebtCalculator.js
@@ -0,0 +1,21 @@
+const getSleepHours = day => {
+  switch (day) {
+    case 'Monday':
+      return 8 + ' hours.';
+    case 'Tuesday':
+      return 8 + ' hours.';
+    case 'Wednesday':
+      return 8 + ' hours.';
+    case 'Thursday':
+      return 8 + ' hours.';
+    case 'Friday':
+      return 8 + ' hours.';
+    case 'Saturday':
+      return 8 + ' hours.';
+    case 'Sunday':
+      return 8 + ' hours.';
+    default:
+      console.log('Error');
+      break;
+  }
+};
